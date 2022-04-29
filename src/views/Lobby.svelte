479a6fa90b91573d85b90d65ecb1bcb397f4eea2
itlackey <itlackey@outlook.com>
2022-04-29 01:50:29 -0500

    intro video example

--- src/views/Lobby.svelte
+++ src/views/Lobby.svelte
@@ -3,15 +3,19 @@
   import JoinGame from "./JoinGame.svelte";
   import StartGame from "./StartGame.svelte";
   import Page from "../components/Page.svelte";
+
 </script>
 
 <style>
 
+
 </style>
 
 <Page title="Dimm City Divination Portal">
+
   <div class="button-container">
     <button on:click="{() => changeView(StartGame)}">Start a new mission</button>
-    <button on:click="{() => changeView(JoinGame)}">Join a current mission</button>
+    <button on:click="{() => changeView(JoinGame)}"
+      >Join a current mission</button>
   </div>
 </Page>
--- src/views/StartGame.svelte
+++ src/views/StartGame.svelte
@@ -9,12 +9,13 @@
   initialSettings.title = "Dimm City RPG - Portal Jumping";
   initialSettings.access = "Public";
   let initalImageUrl = $selectedImageUrl;
-
+  let video;
+  let isPlaying = false;
   function beginGameSession() {
     initialSettings.gameUri = "https://divination.dimm.city/?gid=1234";
     $settings = initialSettings;
     $selectedImageUrl = initalImageUrl;
-    changeView(Board);
+    video.play();
   }
 
   function connectToVideoFeed() {
@@ -24,55 +25,76 @@
 </script>
 
 <style>
- 
-
-
+  video {
+    z-index: -1;
+    position: absolute;
+    width: 100%;
+    margin-left: -1.5rem;
+    opacity: 0;
+    transition: opacity 300ms ease-in;
+  }
+  video.playing {
+    opacity: 1;
+    z-index: 1;
+  }
   .form-container div {
     margin-bottom: 1.5rem;
   }
-
 </style>
 
 <Page title="Configure your mission">
-  <div class="form-container">
-    <div>
-      <label for="title">Name:</label>
-      <input
-        type="text"
-        bind:value="{initialSettings.title}"
-        placeholder="Choose a title for your game session" />
+  <video
+    bind:this="{video}"
+    class:playing="{isPlaying}"
+    on:play="{() => (isPlaying = true)}"
+    on:ended="{() => changeView(Board)}"
+    muted
+    disablepictureinpicture
+    playsinline
+    ><source
+      src="https://files.dimm.city/assets/connecting.mp4"
+      type="video/mp4" /></video>
+  {#if isPlaying == false}
+    <div class="form-container">
+      <div>
+        <label for="title">Name:</label>
+        <input
+          type="text"
+          bind:value="{initialSettings.title}"
+          placeholder="Choose a title for your game session" />
+      </div>
+      <div>
+        <label for="title">Initial location address:</label>
+        <input
+          type="text"
+          placeholder="Initial location address"
+          bind:value="{initalImageUrl}" />
+      </div>
+      <div>
+        <label for="title">Access:</label>
+        <label
+          >Public<input
+            type="radio"
+            bind:group="{initialSettings.access}"
+            value="Public" /></label>
+        <label
+          >Private<input
+            type="radio"
+            bind:group="{initialSettings.access}"
+            value="Private" /></label>
+      </div>
+      <div>
+        {#if initialSettings.mediaFeedUri}
+          <p>You have connected to Vrumble, feed initiated</p>
+        {:else}
+          <p>Please connect to your Vrumble account and select the game feed</p>
+          <a href="#!" on:click="{connectToVideoFeed}">Connect</a>
+        {/if}
+      </div>
     </div>
-    <div>
-      <label for="title">Initial location address:</label>
-      <input
-        type="text"
-        placeholder="Initial location address"
-        bind:value="{initalImageUrl}" />
+    <div class="button-container">
+      <button on:click="{() => changeView(Lobby)}">Back to the lobby...</button>
+      <button on:click="{beginGameSession}">Start the mission!</button>
     </div>
-    <div>
-      <label for="title">Access:</label>
-      <label
-        >Public<input
-          type="radio"
-          bind:group="{initialSettings.access}"
-          value="Public" /></label>
-      <label
-        >Private<input
-          type="radio"
-          bind:group="{initialSettings.access}"
-          value="Private" /></label>
-    </div>
-    <div>
-      {#if initialSettings.mediaFeedUri}
-        <p>You have connected to Vrumble, feed initiated</p>
-      {:else}
-        <p>Please connect to your Vrumble account and select the game feed</p>
-        <a href="#!" on:click="{connectToVideoFeed}">Connect</a>
-      {/if}
-    </div>
-  </div>
-  <div class="button-container">
-    <button on:click="{() => changeView(Lobby)}">Back to the lobby...</button>
-    <button on:click="{beginGameSession}">Start the mission!</button>
-  </div>
+  {/if}
 </Page>
